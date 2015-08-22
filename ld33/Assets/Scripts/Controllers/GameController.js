32fe790c3f9af7b88ff375443c1b9ba6c5c0fefa
Shurty <jeremy.dubuc@epitech.eu>
2015-08-22 12:47:01 +0200

    Added standard lib effects, added UI HUD Play, torus and fixes

---
+++ ld33/Assets/Scripts/Controllers/GameController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+static var activeState: GameState = GameState.LOAD;
+var nextState: GameState = GameState.NONE;
+
+function Start () {
+	nextState = GameState.MAIN_MENU;
+}
+
+function Update () {
+	if (nextState != GameState.NONE) {
+		activeState = nextState;
+		BroadcastMessage("OnStateChanged", activeState);
+		nextState = GameState.NONE;
+	}
+
+	// Local impl.
+	switch (activeState)
+	{
+		case GameState.PLAY_LOADING:
+			nextState = GameState.PLAYING;
+			break;
+		default:
+			break;
+	}
+}
+
+function ChangeState(state: GameState) {
+	nextState = state;
+}
