e3d8115e04ce54c88d47f3eef7529d8393d69232
Baptiste Olivier <olivier.bap@gmail.com>
2015-08-22 11:18:59 +0200

    Fix generation

--- ld33/Assets/Scripts/GameManager.js
+++ ld33/Assets/Scripts/GameManager.js
@@ -17,12 +17,13 @@ private var boardHolder : Transform;
 
 function Start () {
 
+
 	boardHolder = new GameObject ("Board").transform;
 
 	for (var i : int = 0; i < nbCol; i++) {
 		for (var j : int = 0; j < nbRow; j++){
 			var toInstantiate : GameObject = grass[Random.Range (0,grass.Length)];
-			var instance : GameObject = Instantiate (toInstantiate, new Vector3 (i - startPosX, j - startPosY, 0f), Quaternion.identity) as GameObject;
+			var instance : GameObject = Instantiate (toInstantiate, new Vector3 (i + startPosX, j + startPosY, 0f), Quaternion.identity) as GameObject;
             instance.transform.SetParent (boardHolder);
 		}
 	}
