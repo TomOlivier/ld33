87fecf119d7f54dba9e4411d3e415eb603ec2bc9
Baptiste Olivier <olivier.bap@gmail.com>
2015-08-23 23:01:02 +0200

    Ajout prefab de gestionnaire de musique

---
+++ ld33/Assets/Scripts/Controllers/SoundManager.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+
+public var efxSource : AudioSource;
+public var musicSource : AudioSource;
+
+public static var instance : SoundManager = null;
+
+public var titleMusic : AudioClip;
+public var characterMusic : AudioClip;
+public var gameMusic : AudioClip;
+public var rampageMusic : AudioClip;
+
+private var currentMusic : String;
+
+function Awake () {
+
+    if (instance == null)
+        instance = this;
+    else if (instance != this)
+        Destroy (gameObject);
+    
+    DontDestroyOnLoad (gameObject);
+}
+
+public function PlaySfx(clip : AudioClip){
+    
+    efxSource.clip = clip;
+    efxSource.Play ();
+}
+
+public function PlayMusic(musicName : String){
+
+	if(currentMusic == musicName)
+		return;
+
+	if(musicName == "titleMusic"){
+    	musicSource.clip = titleMusic;
+	}
+	else if(musicName == "characterMusic") {
+		musicSource.clip = characterMusic;
+	}
+	else if(musicName == "gameMusic") {
+		musicSource.clip = gameMusic;
+	}
+	else if(musicName == "rampageMusic") {
+		musicSource.clip = rampageMusic;
+	}
+	else
+		return;
+    
+    musicSource.Play ();
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
