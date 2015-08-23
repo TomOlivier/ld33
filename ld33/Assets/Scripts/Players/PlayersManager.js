40803cd629410ffae13d174f15f687e6b5cc781c
Shurty <jeremy.dubuc@epitech.eu>
2015-08-23 14:26:16 +0200

    UI prefabs update improved quality and fixed some issues

--- ld33/Assets/Scripts/Players/PlayersManager.js
+++ ld33/Assets/Scripts/Players/PlayersManager.js
@@ -5,19 +5,32 @@ import System.Collections.Generic;
 public var characters: Character[];
 public var players: Player[];
 public var spawnPositions : Vector2[]; // needs to be as many as players
-	
+public var leaderboard: List.<Player>;
+public var gameLeaderboard: List.<Player>;
+
 function Start () 
 {
 	var ui : ControllerUI = GameObject.Find("Game").GetComponent(ControllerUI);
 
 	for (var pl in players)
 	{
+		pl.character = null;
 		ui.characterSelection.InsertPlayer(pl);
 	}
 
 	for (var pl in characters)
 	{
 		ui.characterSelection.InsertCharacter(pl);
+	}
+}
+
+function ResetAll(hard: boolean) {
+	for (var pl in players)
+	{
+		if (hard)
+			pl.FullReset();
+		else
+			pl.GameReset();
 	}
 }
 
@@ -31,9 +44,10 @@ function Update () {
 function StartGame() {
 	var spawnPositionsCopy : List.<Vector2> = new List.<Vector2>(spawnPositions);
 	
+	CreateGameLeaderboard();
 	for (var pl:Player in players) {
 		if (pl.isActive && pl.playerInstance == null) {
-			pl.GameReset();
+			pl.GameReset();	
 			Debug.Log("new Player");
 			var randomIndex : int = Random.Range(0, spawnPositionsCopy.Count);
 			var randomSpawn : Vector2 = spawnPositionsCopy[randomIndex];
@@ -42,6 +56,12 @@ function StartGame() {
 			pl.playerInstance.GetComponent.<PlayerController>().playerInfo = pl;
 		}
 	}
+}
+
+function EndGame() 
+{	
+	ApplyScoring();
+	CreateLeaderboard();
 }
 
 function FindSubmittingPlayerFromDevice() : Player
@@ -60,4 +80,53 @@ function FindSubmittingPlayerFromDevice() : Player
 function GetPlayer(id: int) : Player
 {
 	return (players[id]);
+}
+
+function ApplyScoring()
+{
+	var scoreInt : int = 4;
+	var prevPlayer : Player;
+
+	for (var i = 0; i < players.length; i++)
+	{
+		gameLeaderboard[i].score += Mathf.Floor(scoreInt);
+		scoreInt /= 2;
+	}
+}
+
+function CreateGameLeaderboard()
+{
+	gameLeaderboard.Clear();
+	for (var i = 0; i < players.length; i++)
+	{
+		gameLeaderboard.Add(players[i]);
+	}
+}
+
+function CreateLeaderboard()
+{
+	var cnt : float = 0.0f;
+
+	leaderboard.Clear();
+	while (cnt < players.length)
+	{
+		var selectedPl : Player = null;
+		
+		for (var i = cnt; i < players.length; i++)
+		{
+			var activePl = players[i];
+
+			if (selectedPl == null)
+				selectedPl = activePl;
+			else if (selectedPl.score < activePl.score && selectedPl.isActive)
+				selectedPl = activePl;
+		}
+		
+		if (selectedPl)
+		{
+			selectedPl.rank = cnt + 1;
+			leaderboard.Add(selectedPl);
+		}
+		cnt++;
+	}
 }
---
+++ ld33/Assets/Scripts/UI/UIGameOver.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+var playersCtrl : PlayersManager;
+var scoresContainer : GameObject;
+var roundNumber : UI.Text;
+var winnerStr : UI.Text;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnShow() {
+	roundNumber.text = "Round " + GameController.roundCount;
+	winnerStr.text = "Player " + playersCtrl.leaderboard[0].uid + " WIN";
+	
+	for (var i = 0; i < 4; i++)
+	{
+		var obj = scoresContainer.transform.Find("PlayerScoreEntry" + i.ToString());
+
+		if (playersCtrl.leaderboard[i].isActive)
+		{
+			obj.gameObject.SetActive(true);
+			obj.Find("PScoreEntryContent/RankIco/Text").GetComponent(UI.Text).text = playersCtrl.leaderboard[i].rank.ToString();
+			obj.Find("PScoreEntryContent/UserInfo/Name").GetComponent(UI.Text).text = "Player " + playersCtrl.leaderboard[i].uid.ToString();
+			obj.Find("PScoreEntryContent/UserInfo/Points").GetComponent(UI.Text).text = playersCtrl.leaderboard[i].score.ToString() + " PTS";			
+		} 
+		else 
+		{
+			obj.gameObject.SetActive(false);
+		}
+	}
+}
