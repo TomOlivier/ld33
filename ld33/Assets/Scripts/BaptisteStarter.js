5885dc3d206ba663513ce85643ae29a47844a3ef
Baptiste Olivier <olivier.bap@gmail.com>
2015-08-23 13:45:31 +0200

    Fix tree generation with 3D

---
+++ ld33/Assets/Scripts/BaptisteStarter.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+
+function Start () {
+
+	var gameManager : GameManager;
+
+	gameManager = this.GetComponent.<GameManager>();
+
+	gameManager.Generate();
+}
+
+function Update () {
+
+}
