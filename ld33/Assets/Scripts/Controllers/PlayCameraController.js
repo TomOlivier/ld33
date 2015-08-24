bc70bb5d73dd05081d406072847acd2021065694
TomOlivier <hello@tomolivier.com>
2015-08-24 16:31:30 +0200

    Lerp on camera

--- ld33/Assets/Scripts/Controllers/PlayCameraController.js
+++ ld33/Assets/Scripts/Controllers/PlayCameraController.js
@@ -2,36 +2,60 @@
 
 var gameController: GameController;
 
-function Start () {
+private var speed : float = 6.0f;
+private var startPosition : Vector3;
+private var endPosition: Vector3;
+private var startTime: float;
+private var journeyLength: float;
 
+function Start () {
+	startTime = Time.time;
+	startPosition = transform.position;
+	journeyLength = Vector3.Distance(startPosition, endPosition);
 }
 
 function Update () {
 	if (gameController.activeState == GameState.PLAYING) {
-		var pos : Vector3;
-		var pltp : Vector3;
-		var cnt : float = 0.0f;
-		var topScale : float = 1.0f;
-		var zDist = 0.0f;
-		
-		for (var pl in gameController.playersManager.players)
-		{
-			if (pl.isActive && pl.isAlive)
-			{
-				var lc = pl.playerInstance.transform.localScale;
 
-				pltp = pl.playerInstance.transform.position;
-				pos += pltp;
-				cnt++;
-				if (lc.x > topScale)
-					topScale = lc.x;
-			}
+		var distCovered = (Time.time - startTime) * speed;
+		var fracJourney = distCovered / journeyLength;
+
+		transform.position = Vector3.Lerp(startPosition, endPosition, fracJourney);
+
+		if (journeyLength - distCovered < Mathf.Epsilon) {
+			startTime = Time.time;
+			startPosition = transform.position;
+			endPosition = GetNewPos();
+			journeyLength = Vector3.Distance(startPosition, endPosition);
 		}
-		pos = new Vector3(pos.x / cnt, pos.y / cnt, 0);
-		pltp.z = 0;
-		zDist = 10.0f + 2.0f * topScale + 1.50f * Mathf.Abs(Vector3.Distance(pos, pltp));
-		pos.y += zDist * 0.1;
-
-		this.transform.position = new Vector3(pos.x, pos.y, zDist);
 	}
+}
+
+function GetNewPos () {
+	var pos : Vector3;
+	var pltp : Vector3;
+	var cnt : float = 0.0f;
+	var topScale : float = 1.0f;
+	var zDist = 0.0f;
+
+	for (var pl in gameController.playersManager.players)
+	{
+		if (pl.isActive && pl.isAlive)
+		{
+			var lc = pl.playerInstance.transform.localScale;
+
+			pltp = pl.playerInstance.transform.position;
+			pos += pltp;
+			cnt++;
+			if (lc.x > topScale)
+				topScale = lc.x;
+		}
+	}
+
+	pos = new Vector3(pos.x / cnt, pos.y / cnt, 0);
+	pltp.z = 0;
+	zDist = 10.0f + 2.0f * topScale + 1.50f * Mathf.Abs(Vector3.Distance(pos, pltp));
+	pos.y += zDist * 0.1;
+
+	return new Vector3(pos.x, pos.y, zDist);
 }
