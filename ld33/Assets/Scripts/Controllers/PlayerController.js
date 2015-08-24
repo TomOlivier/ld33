e8e733f8c13d8554428f0b6ec50e7d466e2455c4
Shurty <jeremy.dubuc@epitech.eu>
2015-08-24 16:32:42 +0200

    Forces and UI

--- ld33/Assets/Scripts/Controllers/PlayerController.js
+++ ld33/Assets/Scripts/Controllers/PlayerController.js
@@ -5,7 +5,7 @@ import System.Collections.Generic;
 
 public var speed : float = 5;
 public var spriteRenderer : SpriteRenderer = null;
-public var pushStrength : float = 25;
+public var pushStrength : float = 10;
 public var weakness : float = 20; // the higher, the more it will the pushed
 public var attackCooldownDef : float = 0.5;
 public var eatCooldownDef : float = 0.25;
